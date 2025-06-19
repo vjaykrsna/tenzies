d345a3917e4df71c74ee64bd2b62d3f8cca5c9f2
vjaykrsna <vijay74684@gmail.com>
2025-06-19 18:06:25 +0530

    made changes for github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import ghPages from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), ghPages()],
+  plugins: [react()],
   base: '/tenzies/'
 })
