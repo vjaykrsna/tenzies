be715ec7b24c6fd7a08a30d163524aee3f13354e
vjaykrsna <vijay74684@gmail.com>
2025-06-26 18:02:05 +0530

    added github repo link in the footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 import Die from "./Die";
 import { nanoid } from "nanoid";
-// import { useWindowSize } from "@react-hook/window-size";
 import Confetti from "react-confetti";
 
 export default function App() {
     const [dice, setDice] = useState(generateDiceValues);
-    // const { width, height } = useWindowSize();
 
     function generateDiceValues() {
         return Array(10)
@@ -22,12 +20,12 @@ export default function App() {
         gameWon
             ? setDice(generateDiceValues)
             : setDice((prevDice) =>
-                  prevDice.map((die) =>
-                      die.isHeld
-                          ? die
-                          : { ...die, value: Math.ceil(Math.random() * 6) }
-                  )
-              );
+                prevDice.map((die) =>
+                    die.isHeld
+                        ? die
+                        : { ...die, value: Math.ceil(Math.random() * 6) }
+                )
+            );
     }
 
     function handleClick(id) {
@@ -47,17 +45,27 @@ export default function App() {
     ));
 
     return (
-        <main>
-            {gameWon && <Confetti />}
-            <h1>Tenzies</h1>
-            <p>
-                Roll untill all dice are same. Click each die to freeze it to
-                its current value between rolls.
-            </p>
-            <div className="dice-container">{dieBtnList}</div>
-            <button className="roll-btn" onClick={handleRoll}>
-                {gameWon ? "New Game" : "Roll"}
-            </button>
-        </main>
+        <>
+            <main>
+                {gameWon && <Confetti />}
+                <h1>Tenzies</h1>
+                <p>
+                    Roll untill all dice are same. Click each die to freeze it to
+                    its current value between rolls.
+                </p>
+                <div className="dice-container">{dieBtnList}</div>
+                <button className="roll-btn" onClick={handleRoll}>
+                    {gameWon ? "New Game" : "Roll"}
+                </button>
+            </main>
+            <footer>
+                <a href="https://github.com/vjaykrsna/tenzies" target="_blank" rel="noopener noreferrer" className="github-link">
+                    <svg viewBox="0 0 16 16"className="github-icon">
+                        <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.19.01-.82.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07.01 1.93.01 2.2 0 .21-.15.46-.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8Z"></path>
+                    </svg>
+                    GitHub Repository
+                </a>
+            </footer>
+        </>
     );
 }
